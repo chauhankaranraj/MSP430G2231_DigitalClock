f68c7038d63680fa6b538242fb1d4df4d24e56c8
Karanraj Chauhan <chauhank@bu.edu>
2017-01-17 16:35:37 -0500

    Completed display updating code. TODO: implement timer

--- main.c
+++ main.c
@@ -1,39 +1,38 @@
 #include <msp430g2231.h>
 
-//#define SRCLK P1.0
-//#define SER1 P1.1
-//#define SER2 P1.2
-//#define SER3 P1.3
-//#define SER4 P1.4
-//#define RCLK P1.5
+// SRCLK P1.0
+// SER1 P1.1
+// SER2 P1.2
+// SER3 P1.3
+// SER4 P1.4
+// RCLK P1.5
 
 // Led 1-7 of seven segment display correspond to outputs A-G of bit shift register
-
-//void updateSecondsUnitsDisplay() {}
-//
-//void updateSecondsTensDisplay(){}
-//
-//void updateMintuesUnitsDisplay(){}
-//
-//void updateMintuesTensDisplay(){}
 
 int main(void) {
     // TODO: update watch dog timer to make clock display go back to 0000 when the day is over
     WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
 
-    P1OUT = 0;
+    BCSCTL1 = CALBC1_1MHZ;
+    DCOCTL = CALDCO_1MHZ;
+
+    P2OUT = 0;
+    P2DIR = 0;
+
+    P1OUT = 0;  // setting all gpio to zero initially
     P1DIR = 1 + 2 + 4 + 8 + 16 + 32; //  P1.0 - P1.5 are outputs, P1.6 and P1.7 are not being used
 
-    const char digit0[] = {BIT1, BIT1, BIT1, BIT1, BIT1, BIT1, BIT0};
-    const char digit1[] = {BIT0, BIT1, BIT1, BIT0, BIT0, BIT0, BIT0};
-    const char digit2[] = {BIT1, BIT1, BIT0, BIT1, BIT1, BIT0, BIT1};
-    const char digit3[] = {BIT1, BIT1, BIT1, BIT1, BIT0, BIT0, BIT1};
-    const char digit4[] = {BIT0, BIT1, BIT1, BIT0, BIT0, BIT1, BIT1};
-    const char digit5[] = {BIT1, BIT0, BIT1, BIT1, BIT0, BIT1, BIT1};
-    const char digit6[] = {BIT1, BIT0, BIT1, BIT1, BIT1, BIT1, BIT1};
-    const char digit7[] = {BIT1, BIT1, BIT1, BIT0, BIT0, BIT0, BIT0};
-    const char digit8[] = {BIT1, BIT1, BIT1, BIT1, BIT1, BIT1, BIT1};
-    const char digit9[] = {BIT1, BIT1, BIT1, BIT1, BIT0, BIT1, BIT1};
+    // index i of array digitx denotes whether led i will be on or off for the digitx
+    const char digit0[] = {1, 1, 1, 1, 1, 1, 0};
+    const char digit1[] = {0, 1, 1, 0, 0, 0, 0};
+    const char digit2[] = {1, 1, 0, 1, 1, 0, 1};
+    const char digit3[] = {1, 1, 1, 1, 0, 0, 1};
+    const char digit4[] = {0, 1, 1, 0, 0, 1, 1};
+    const char digit5[] = {1, 0, 1, 1, 0, 1, 1};
+    const char digit6[] = {1, 0, 1, 1, 1, 1, 1};
+    const char digit7[] = {1, 1, 1, 0, 0, 0, 0};
+    const char digit8[] = {1, 1, 1, 1, 1, 1, 1};
+    const char digit9[] = {1, 1, 1, 1, 0, 1, 1};
 
     const char* digits[10];
     digits[0] = digit0;
@@ -54,39 +53,48 @@ int main(void) {
 
     while(1) {
         // wait for timer for completion of one minute
+        _delay_cycles(1000000);
 
-        // update seconds units display
+        // update shift registers with values
         signed char led;
         for (led = 6; led >= 0; led--) {
-            P1OUT |= digits[secondsUnitsDigitIndex][led];   // set ser1 to the current bit for the current digit
-            _delay_cycles(1);   // time delay between ser1 change and srclk high
+            P1OUT = digits[secondsUnitsDigitIndex][led]*2 + digits[secondsTensDigitIndex][led]*4
+                    + digits[minutesUnitsDigitIndex][led]*8 + digits[minutesTensDigitIndex][led]*16;   // set ser1 to thecurrent bit for the current digit
+            _delay_cycles(1);   // time delay between ser change and srclk high
             P1OUT |= BIT0;  // srclk high
             _delay_cycles(1);   // pulse duration for srclk high
             P1OUT &= ~(BIT0);   // srclk low
         }
+
+        // set output to new values in shift registers
         _delay_cycles(1);   // time delay between srclk pulse and rclk high
-        P1OUT |= BIT5;
-        _delay_cycles(1);
-        P1OUT &= ~(BIT5);
+        P1OUT |= BIT5;  // rclk high
+        _delay_cycles(1);   // pulse duration for rclk high
+        P1OUT &= ~(BIT5);   // rclk low
 
-        //TODO: put this line of code at the end
+        // update seconds units digit display index every second,
+        // update seconds tens digit display index when seconds units digit display index passes 9
+        // update minutes units digit display index when seconds tens digit display index passes 5
+        // update minutes tens digit display index when minutes units digit display index passes 9
+        // reset all indices when 59:59 is reached
         secondsUnitsDigitIndex++;
-
-
-        // TODO: when srclkis high the first regs=ister will read data as well, even though only the second register should be reading
-
-
-
-
-        // update seconds tens display
         if (secondsUnitsDigitIndex == 10) {
             secondsUnitsDigitIndex = 0;
-            P1OUT |= digits[secondsTensDigitIndex][led];   // set ser1 to the current bit for the current digit
-            _delay_cycles(1);   // time delay between ser1 change and srclk high
-            P1OUT |= BIT0;  // srclk high
-            _delay_cycles(1);   // pulse duration for srclk high
-            P1OUT &= ~(BIT0);   // srclk low
+            secondsTensDigitIndex++;
+            if (secondsTensDigitIndex == 6) {
+                secondsTensDigitIndex = 0;
+                minutesUnitsDigitIndex++;
+                if (minutesUnitsDigitIndex == 10) {
+                    minutesUnitsDigitIndex = 0;
+                    minutesTensDigitIndex++;
+                    if (minutesTensDigitIndex == 6) {
+                        minutesTensDigitIndex = 0;
+                        secondsUnitsDigitIndex = 0;
+                        secondsTensDigitIndex = 0;
+                        minutesUnitsDigitIndex = 0;
+                    }
+                }
+            }
         }
-
     }
 }
