5cd0940149c134b936925355ec3ae380e3bf2650
Karanraj Chauhan <chauhank@bu.edu>
2017-01-18 01:10:47 -0500

    Timer using _delay_cycles() implemented

--- main.c
+++ main.c
@@ -9,27 +9,6 @@
 
 // Led 1-7 of seven segment display correspond to outputs A-G of bit shift register
 
-// index i of array digitx denotes whether led i will be on or off for the digitx
-const char digit0[] = {1, 1, 1, 1, 1, 1, 0};
-const char digit1[] = {0, 1, 1, 0, 0, 0, 0};
-const char digit2[] = {1, 1, 0, 1, 1, 0, 1};
-const char digit3[] = {1, 1, 1, 1, 0, 0, 1};
-const char digit4[] = {0, 1, 1, 0, 0, 1, 1};
-const char digit5[] = {1, 0, 1, 1, 0, 1, 1};
-const char digit6[] = {1, 0, 1, 1, 1, 1, 1};
-const char digit7[] = {1, 1, 1, 0, 0, 0, 0};
-const char digit8[] = {1, 1, 1, 1, 1, 1, 1};
-const char digit9[] = {1, 1, 1, 1, 0, 1, 1};
-
-const char* digits[10];
-
-char secondsUnitsDigitIndex = 0;
-char secondsTensDigitIndex = 0;
-char minutesUnitsDigitIndex = 0;
-char minutesTensDigitIndex = 0;
-
-char timeCounter = 0;
-
 int main(void){
 
     // TODO: update watch dog timer to make clock display go back to 0000 when the day is over
@@ -44,6 +23,19 @@ int main(void){
     P1OUT = 0;  // setting all gpio to zero initially
     P1DIR = 1 + 2 + 4 + 8 + 16 + 32; //  P1.0 - P1.5 are outputs, P1.6 and P1.7 are not being used
 
+    // index i of array digitx denotes whether led i will be on or off for the digitx
+    const char digit0[] = {1, 1, 1, 1, 1, 1, 0};
+    const char digit1[] = {0, 1, 1, 0, 0, 0, 0};
+    const char digit2[] = {1, 1, 0, 1, 1, 0, 1};
+    const char digit3[] = {1, 1, 1, 1, 0, 0, 1};
+    const char digit4[] = {0, 1, 1, 0, 0, 1, 1};
+    const char digit5[] = {1, 0, 1, 1, 0, 1, 1};
+    const char digit6[] = {1, 0, 1, 1, 1, 1, 1};
+    const char digit7[] = {1, 1, 1, 0, 0, 0, 0};
+    const char digit8[] = {1, 1, 1, 1, 1, 1, 1};
+    const char digit9[] = {1, 1, 1, 1, 0, 1, 1};
+
+    const char* digits[10];
     digits[0] = digit0;
     digits[1] = digit1;
     digits[2] = digit2;
@@ -55,61 +47,53 @@ int main(void){
     digits[8] = digit8;
     digits[9] = digit9;
 
-    CCTL0 = CCIE;
-    CCR0 = 49999;  //FIXME: CCR0 cannot take numbers bigger than 999980?
-    TACTL = TASSEL_2 + MC_1;
-    __bis_SR_register(GIE+LPM0_bits);
-    // wait for timer for completion of 0.99998 seconds, i.e., reach CCR0 value of 49999 20 times
-}
+    char secondsUnitsDigitIndex = 0;
+    char secondsTensDigitIndex = 0;
+    char minutesUnitsDigitIndex = 0;
+    char minutesTensDigitIndex = 0;
 
-#pragma vector = TIMERA0_VECTOR
-__interrupt void Timer_A (void) {
+    while(1) {
+        _delay_cycles(999980);  // create delay of 0.99998 seconds
 
-    timeCounter++;
-
-    if (timeCounter == 20) {
-        // update shift registers with values
         signed char led;
         for (led = 6; led >= 0; led--) {
-            P1OUT = digits[secondsUnitsDigitIndex][led]*2 + digits[secondsTensDigitIndex][led]*4
-                    + digits[minutesUnitsDigitIndex][led]*8 + digits[minutesTensDigitIndex][led]*16;   // set ser1 to thecurrent bit for the current digit
-            _delay_cycles(1);   // time delay between ser change and srclk high
-            P1OUT |= BIT0;  // srclk high
-            _delay_cycles(1);   // pulse duration for srclk high
-            P1OUT &= ~(BIT0);   // srclk low
+           P1OUT = digits[secondsUnitsDigitIndex][led]*2 + digits[secondsTensDigitIndex][led]*4
+                   + digits[minutesUnitsDigitIndex][led]*8 + digits[minutesTensDigitIndex][led]*16;   // set ser1 to thecurrent bit for the current digit
+           _delay_cycles(1);   // time delay between ser change and srclk high
+           P1OUT |= BIT0;  // srclk high
+           _delay_cycles(1);   // pulse duration for srclk high
+           P1OUT &= ~(BIT0);   // srclk low
         }
 
         // set output to new values in shift registers
-        _delay_cycles(1);   // time delay between srclk pulse and rclk high
-        P1OUT |= BIT5;  // rclk high
-        _delay_cycles(1);   // pulse duration for rclk high
-        P1OUT &= ~(BIT5);   // rclk low
+       _delay_cycles(1);   // time delay between srclk pulse and rclk high
+       P1OUT |= BIT5;  // rclk high
+       _delay_cycles(1);   // pulse duration for rclk high
+       P1OUT &= ~(BIT5);   // rclk low
 
-        // update seconds units digit display index every second,
-        // update seconds tens digit display index when seconds units digit display index passes 9
-        // update minutes units digit display index when seconds tens digit display index passes 5
-        // update minutes tens digit display index when minutes units digit display index passes 9
-        // reset all indices when 59:59 is reached
-        secondsUnitsDigitIndex++;
-        if (secondsUnitsDigitIndex == 10) {
-            secondsUnitsDigitIndex = 0;
-            secondsTensDigitIndex++;
-            if (secondsTensDigitIndex == 6) {
-                secondsTensDigitIndex = 0;
-                minutesUnitsDigitIndex++;
-                if (minutesUnitsDigitIndex == 10) {
-                    minutesUnitsDigitIndex = 0;
-                    minutesTensDigitIndex++;
-                    if (minutesTensDigitIndex == 6) {
-                        minutesTensDigitIndex = 0;
-                        secondsUnitsDigitIndex = 0;
-                        secondsTensDigitIndex = 0;
-                        minutesUnitsDigitIndex = 0;
-                    }
-                }
-            }
-        }
-
-        timeCounter = 0;
+       // update seconds units digit display index every second,
+       // update seconds tens digit display index when seconds units digit display index passes 9
+       // update minutes units digit display index when seconds tens digit display index passes 5
+       // update minutes tens digit display index when minutes units digit display index passes 9
+       // reset all indices when 59:59 is reached
+       secondsUnitsDigitIndex++;
+       if (secondsUnitsDigitIndex == 10) {
+           secondsUnitsDigitIndex = 0;
+           secondsTensDigitIndex++;
+           if (secondsTensDigitIndex == 6) {
+               secondsTensDigitIndex = 0;
+               minutesUnitsDigitIndex++;
+               if (minutesUnitsDigitIndex == 10) {
+                   minutesUnitsDigitIndex = 0;
+                   minutesTensDigitIndex++;
+                   if (minutesTensDigitIndex == 6) {
+                       minutesTensDigitIndex = 0;
+                       secondsUnitsDigitIndex = 0;
+                       secondsTensDigitIndex = 0;
+                       minutesUnitsDigitIndex = 0;
+                   }
+               }
+           }
+       }
     }
 }
