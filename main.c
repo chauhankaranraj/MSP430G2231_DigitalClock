e4e7a5d31f8bf9d5fd74cb02dc0f3309b64b2fed
karan2019 <chauhank@bu.edu>
2017-01-21 03:36:17 -0500

    Update main.c
    All displays work as required. FIXME: Make time display accurate

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <msp430g2231.h>
 
-// CLK P1.0
-// SER A P1.1
+// P1.0 CLK
+// P1.1 SER seconds units
+// P1.2 SER seconds tens
 
 // index+1 of array digitx denotes whether led i will be on or off for the digitx
 const char digit0[] = {1, 1, 1, 1, 1, 1, 0, 0};
@@ -18,9 +19,14 @@ const char digit9[] = {1, 1, 1, 1, 0, 1, 1, 0};
 const char* digits[10];
 
 char secondsUnitsDigitIndex = 0;
+char secondsTensDigitIndex = 0;
+char minutesUnitsDigitIndex = 0;
+char minutesTensDigitIndex = 0;
+
+char timeCounter = 0;
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watch dog timer
+    WDTCTL = WDTPW + WDTHOLD;	// Stop watch dog timer
 	
     BCSCTL1 = CALBC1_1MHZ;
     DCOCTL = CALDCO_1MHZ;
@@ -29,7 +35,7 @@ int main(void) {
     P2DIR = 0;
 
     P1OUT = 0;  // setting all gpio to zero initially
-    P1DIR |= (BIT0 + BIT1); //  P1.0 - P1.5 are outputs, P1.6 and P1.7 are not being used
+    P1DIR |= (BIT0 + BIT1 + BIT2 + BIT3 + BIT4); //  P1.0 - P1.5 are outputs, P1.6 and P1.7 are not being used
 
     digits[0] = digit0;
     digits[1] = digit1;
@@ -42,22 +48,52 @@ int main(void) {
     digits[8] = digit8;
     digits[9] = digit9;
 
-    signed char led;
-    while(1) {
+    CCTL0 = CCIE;
+    CCR0 = 49999;  //FIXME: CCR0 cannot take numbers bigger than 999980?
+    TACTL = TASSEL_2 + MC_1;
+    __bis_SR_register(GIE+LPM0_bits);
+    // wait for timer for completion of 0.99998 seconds, i.e., reach CCR0 value of 49999 20 times
+}
+
+#pragma vector = TIMERA0_VECTOR
+__interrupt void Timer_A (void) {
+
+    timeCounter++;
+
+    if (timeCounter == 20) {
+        // update shift registers with values
+        signed char led;
         for (led = 7; led >= 0; led--) {
-            P1OUT = digits[secondsUnitsDigitIndex][led]*2;
+            P1OUT = digits[secondsUnitsDigitIndex][led]*2 + digits[secondsTensDigitIndex][led]*4
+                    + digits[minutesUnitsDigitIndex][led]*8 + digits[minutesTensDigitIndex][led]*16;;
             _delay_cycles(0.025);   // time delay between ser change and srclk high = 25ns
             P1OUT |= BIT0;  // clk high
             _delay_cycles(0.025);   // pulse duration for srclk high = 25ns
             P1OUT &= ~(BIT0);   // clk low
         }
 
-        _delay_cycles(1000000);
-
-        if (secondsUnitsDigitIndex==9) {
+        secondsUnitsDigitIndex++;
+        if (secondsUnitsDigitIndex == 10) {
             secondsUnitsDigitIndex = 0;
-        } else {
-            secondsUnitsDigitIndex++;
+            secondsTensDigitIndex++;
+            if (secondsTensDigitIndex == 6) {
+                secondsTensDigitIndex = 0;
+                minutesUnitsDigitIndex++;
+                if (minutesUnitsDigitIndex == 10) {
+                    minutesUnitsDigitIndex = 0;
+                    minutesTensDigitIndex++;
+                    if (minutesTensDigitIndex == 6) {
+                        minutesTensDigitIndex = 0;
+                        secondsUnitsDigitIndex = 0;
+                        secondsTensDigitIndex = 0;
+                        minutesUnitsDigitIndex = 0;
+                    }
+                }
+            }
+
         }
+
+        timeCounter = 0;
     }
+
 }
